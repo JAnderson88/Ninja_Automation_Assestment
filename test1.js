f1680af9ffea1f40a72f5695595400a130b8177b
JAnderson88 <jande081@fiu.edu>
2020-02-20 03:56:28 -0500

    uncommented code

--- test1.js
+++ test1.js
@@ -7,22 +7,22 @@ const url = 'http://localhost:3001';
 fixture('test1')
   .page(url)
 
-// test('test1', async t => {
-//   let list = await APICalls.getDevices();
-//   let boxes = await Selector('.device-options');
-//   let names = await Selector('.device-name');
-//   let types = await Selector('.device-type');
-//   let capacities = await Selector('.device-capacity');
-//   for (let i = 0; i < list.length; i++) {
-//     const { system_name, type, hdd_capacity } = list[i];
-//     await t
-//       .expect(names.nth(i).withText(system_name)).ok()
-//       .expect(types.nth(i).withText(type)).ok()
-//       .expect(capacities.nth(i).withText(hdd_capacity)).ok()
-//       .expect(boxes.nth(i).find('a').visible).ok()
-//       .expect(boxes.nth(i).find('button').visible).ok()
-//   }
-// });
+test('test1', async t => {
+  let list = await APICalls.getDevices();
+  let boxes = await Selector('.device-options');
+  let names = await Selector('.device-name');
+  let types = await Selector('.device-type');
+  let capacities = await Selector('.device-capacity');
+  for (let i = 0; i < list.length; i++) {
+    const { system_name, type, hdd_capacity } = list[i];
+    await t
+      .expect(names.nth(i).withText(system_name)).ok()
+      .expect(types.nth(i).withText(type)).ok()
+      .expect(capacities.nth(i).withText(hdd_capacity)).ok()
+      .expect(boxes.nth(i).find('a').visible).ok()
+      .expect(boxes.nth(i).find('button').visible).ok()
+  }
+});
 
 test('test2', async t => {
   //Selectors
